12d27ff2ceb0e4ab50dbe6f5ac3cc2df5cf319be
infopreet29 <infopreet@gmail.com>
2026-07-28 09:14:49 +0530

    Add files via upload

---
+++ Website_B/ListBox.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage.master" AutoEventWireup="false" CodeFile="ListBox.aspx.vb" Inherits="ListBox" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cph1" Runat="Server">
+    <form id="form1" runat="server">
+<br />
+
+<center>
+    <h3>LISTBOX DEMO</h3>
+    <br />
+    <asp:ListBox ID="ListBox1" runat="server" Height="202px" Width="229px">
+    </asp:ListBox>
+    <br />
+    <br />
+    <br />
+
+    
+</center>
+
+</form>
+
+</asp:Content>
+
---
+++ Website_B/ListBox.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Partial Class ListBox
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If IsPostBack = False Then
+            FillData()
+        End If
+    End Sub
+    Private Sub FillData()
+        Dim cn As New SqlConnection(ConfigurationManager.ConnectionStrings("MyDB").ToString)
+        Dim cmd As New SqlCommand
+        Dim ds As New DataSet
+        Dim da As New SqlDataAdapter
+        cmd.CommandText = "SELECT ENAME,mobile FROM EMPMST ORDER BY ENAME"
+        cmd.Connection = cn
+        da.SelectCommand = cmd
+        cn.Open()
+        da.Fill(ds) 'Execute query in DB and fill records in dataset
+        cn.Close()
+        ListBox1.DataValueField = "ename"
+        ListBox1.DataSource = ds.Tables(0)
+        ListBox1.DataBind()
+        da.Dispose() : cmd.Dispose() : cn.Dispose()
+    End Sub
+End Class
